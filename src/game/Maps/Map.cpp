4ef5d4afb2b9e347e299972e5918b5f676b7998f
celguar <celguar@gmail.com>
2021-07-11 00:20:21 +0300

    fix function name

--- src/game/Maps/Map.cpp
+++ src/game/Maps/Map.cpp
@@ -836,7 +836,7 @@ bool Map::HasRealPlayers()
         if (!player || !player->IsInWorld())
             continue;
 
-        if (!player->GetPlayerbotAI() || player->GetPlayerbotAI()->isRealPlayer())
+        if (!player->GetPlayerbotAI() || player->GetPlayerbotAI()->IsRealPlayer())
         {
             return true;
         }
