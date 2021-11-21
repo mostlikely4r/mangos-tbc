9ff8681cc3e9654037cc71da114f2c58194f64c8
celguar <celguar@gmail.com>
2021-11-22 02:31:02 +0300

    Custom stuff
    Instant taxi option
    Option to make players on taxi visible from afar
    Check roles based on talents for LFG
    set expansion to tbc if not provided
    Save some info in armory feed (Includes bosses killed, items)
    Save more stats to db
    Start arena season event based on config
    SWP: Ice & Fire barrier fix
    Solocraft patch
    Fix arena join
    Fix memory leak
    Fix visibility of far objects
    Scourge Invasion: Fix necropoli visibility
    Fix Scourge Invasion mail spam
    Letter should not be sent on server start, only on initial event start
    Only show full who list if gm on
    
    Co-Authored-By: Niam5 <517381+Niam5@users.noreply.github.com>

--- src/game/Entities/Unit.cpp
+++ src/game/Entities/Unit.cpp
@@ -1454,6 +1454,11 @@ void Unit::JustKilledCreature(Unit* killer, Creature* victim, Player* responsibl
             if (map->IsRaidOrHeroicDungeon() && victim->GetCreatureInfo()->ExtraFlags & CREATURE_EXTRA_FLAG_INSTANCE_BIND)
             {
                 static_cast<DungeonMap*>(map)->PermBindAllPlayers(creditedPlayer);
+
+                /* World of Warcraft Armory */
+                if (creditedPlayer)
+                    creditedPlayer->CreateWowarmoryFeed(3, victim->GetCreatureInfo()->Entry, 0, 0);
+                /* World of Warcraft Armory */
             }
             static_cast<DungeonMap*>(map)->GetPersistanceState()->UpdateEncounterState(ENCOUNTER_CREDIT_KILL_CREATURE, victim->GetEntry());
         }
@@ -7697,6 +7702,9 @@ int32 Unit::SpellBaseDamageBonusDone(SpellSchoolMask schoolMask)
 
     if (GetTypeId() == TYPEID_PLAYER)
     {
+        // Base value
+        DoneAdvertisedBenefit += ((Player*)this)->GetBaseSpellPowerBonus();
+
         // Damage bonus from stats
         AuraList const& mDamageDoneOfStatPercent = GetAurasByType(SPELL_AURA_MOD_SPELL_DAMAGE_OF_STAT_PERCENT);
         for (auto i : mDamageDoneOfStatPercent)
@@ -7915,6 +7923,9 @@ int32 Unit::SpellBaseHealingBonusDone(SpellSchoolMask schoolMask)
     // Healing bonus of spirit, intellect and strength
     if (GetTypeId() == TYPEID_PLAYER)
     {
+        // Base value
+        AdvertisedBenefit += ((Player*)this)->GetBaseSpellPowerBonus();
+
         // Healing bonus from stats
         AuraList const& mHealingDoneOfStatPercent = GetAurasByType(SPELL_AURA_MOD_SPELL_HEALING_OF_STAT_PERCENT);
         for (auto i : mHealingDoneOfStatPercent)
